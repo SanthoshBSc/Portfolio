33b730c96b848d7f2f9c4e9ed8ef00054400aeb2
Santhosh M <149255802+SanthoshBSc@users.noreply.github.com>
2025-07-16 16:13:28 +0530

    Delete script.js
    old ve

--- script.js
+++
@@ -1,9 +0,0 @@
-// Example JS for form submission
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    alert("Thank you for reaching out!");
-    form.reset();
-  });
-});
